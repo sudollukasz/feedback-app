5e7ea2e9a35c6da7a4488615e556dbe1127a9699
Łukasz Sudoł <sudol.lukasz@gmail.com>
2018-02-27 22:28:15 +0100

    billing added

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 class Header extends Component {
   renderConntent() {
@@ -9,27 +10,30 @@ class Header extends Component {
         return;
       case false:
         return (
-          <li>
+          <li key="Login">
             <a href="/auth/google">Login with Google</a>
           </li>
         );
       default:
-        return (
-          <li>
+        return [
+          <li key="1">
+            <Payments />
+          </li>,
+          <li key="3" style={{ margin: '0 10px' }}>
+            Credits: {this.props.auth.credits}
+          </li>,
+          <li key="2">
             <a href="/api/logout">Logout</a>
           </li>
-        );
+        ];
     }
   }
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
-          <Link 
-          to={this.props.auth ? '/surveys' : '/' } 
-          className="brand-logo"
-          >
-          Feedback
+          <Link to={this.props.auth ? '/surveys' : '/'} className="brand-logo">
+            Feedback
           </Link>
           <ul className="right hide-on-med-and-down">{this.renderConntent()}</ul>
         </div>
