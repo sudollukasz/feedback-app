54330b0bcdb0603eb9ba103c86b6c06457718db1
Łukasz Sudoł <sudol.lukasz@gmail.com>
2018-03-01 17:18:51 +0100

    sendgrid and forms

---
+++ client/src/components/surveys/SurveyField.js
@@ -0,0 +1,10 @@
+import React from 'react'
+export default ({ input, label, meta: { touched, error }}) => {
+    return (
+        <div>
+            <label>{label}</label>
+            <input {...input} style={{marginBottom: '5px'}}/>
+            <div className="red-text" style={{marginBottom: '20px'}}>{touched && error}</div>
+        </div>
+    )
+}
--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
-import materializeCSS from 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/css/materialize.min.css';
 
 import App from './components/App';
 import reducers from './reducers';
+
+//dev
+// import axios from 'axios';
+// window.axios = axios;
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
